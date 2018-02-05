fe76fc9521ba80a5da9f57765101a096793aaa95
DanielSecco <33034487+DanielSecco@users.noreply.github.com>
2018-02-05 17:26:07 -0200

    Initial commit

---
+++ src/components/comps/Database.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="imgf" class="database">
+    <h1>{{ msg }}</h1>
+   
+    
+  </div>
+</template>
+
+<script>
+	//var tata = furlf;
+
+export default {
+  name: 'database',
+  data () {
+    return {
+      msg: 'Imagens on Firebase',
+		
+		
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+    color:beige;
+}
+
+</style>
